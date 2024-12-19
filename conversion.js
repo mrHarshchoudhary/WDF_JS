e7da3f532afbf331e3800ca4b5c3305649d9f1d0
mrHarshchoudhary <Harshchoudharyv@gmail.com>
2024-12-19 11:35:56 +0530

    change here

--- conversion.js
+++ conversion.js
@@ -18,3 +18,31 @@ function updatePlaceholder() {
             input.placeholder = 'Enter value';
     }
 }
+
+function converter() {
+    const value = parseFloat(document.getElementById('val').value);
+    const type = document.getElementById('division').value;
+    let result;
+
+    if (isNaN(value)) {
+        result = "Please enter a valid number";
+    } else {
+        switch (type) {
+            case 'cel':
+                result = `${(value * 9 / 5 + 32).toFixed(2)} °F`;
+                break;
+            case 'fah':
+                result = `${((value - 32) * 5 / 9).toFixed(2)} °C`;
+                break;
+            case 'rupee':
+                result = `₹ ${(value * 82).toFixed(2)}`;
+                break;
+            case 'dollar':
+                result = `$ ${(value / 82).toFixed(2)}`;
+                break;
+            default:
+                result = "Please select a conversion type";
+        }
+    }
+    document.getElementById('Result').innerText = result;
+}
